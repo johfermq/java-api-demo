9cdec37215bf66f8cff5502da756e895215e14da
johfermq <johfermq@hotmail.com>
2024-02-17 20:28:58 -0500

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        log.info("Log: Bienvenido al home de java 17");
+
+        return "Bienvenido al home de java 17";
+    }
+}
